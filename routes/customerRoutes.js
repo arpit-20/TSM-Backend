acc0c015c9899fa444afc1186316ce9ee1315a7e
Arpit <arp@publicisgroupe.net>
2022-06-10 17:36:27 +0530

    node mailer added again

--- routes/customerRoutes.js
+++ routes/customerRoutes.js
@@ -9,13 +9,13 @@ const customersData = require('../models/customers');
 
 var nodeMail = require('../logics/blogic');
 
-this.mailOptions={}
+this.mailOptions = {}
 
 router.get('/', async (req, res) => {
 
     try {
-        const consumers = await customersData.find();
-        res.status(200).json(consumers);
+        const customers = await customersData.find();
+        res.status(200).json(customers);
 
     } catch (error) {
         console.log(" get error " + " " + error);
@@ -30,8 +30,8 @@ router.get('/:id', async (req, res) => {
     console.log('Get request by id');
 
     try {
-        const consumer = await customersData.findById(req.params.id) //callback
-        res.status(200).json(consumer);
+        const customer = await customersData.findById(req.params.id) //callback
+        res.status(200).json(customer);
 
     } catch (err) {
         console.log("get error" + err);
@@ -49,9 +49,9 @@ router.post('/', async (req, res) => {
 
         name: req.body.name,
         email: req.body.email,
-        purchase_form:req.body.purchase_form,
-        service_form:req.body.service_form,
-        contact_form:req.body.contact_form,
+        purchase_form: req.body.purchase_form,
+        service_form: req.body.service_form,
+        contact_form: req.body.contact_form,
         team_name: req.body.team_name,
         mobile_number: req.body.mobile_number,
         shocker_brand: req.body.shocker_brand,
@@ -64,34 +64,104 @@ router.post('/', async (req, res) => {
         other_requirement: req.body.other_requirement
     })
 
-    
-        console.log("post consumer" + customer);
 
-    await customer.save().then((customerData)=>{
-        res.status(201).json(customerData);
-        
-        }).catch(err=>{
+    console.log("post customer" + customer);
 
-        res.status(400).send("Error" + err)
+    await customer.save().then((customerData) => {
+        if (req.body.service_form == true) {
+
+            console.log("service form");
+            this.mailOptions = {
+                from: 'Mail.theshockmechanica@gmail.com',
+                to: ['arpitghai20@gmail.com', 'theshockmechanica@gmail.com'],
+                cc: req.body.email,
+                subject: 'Test node mail',
+                text: `Hi` + ' ' + `${req.body.name}` + "\n" +
+                    `This is an automated mail confirming your service request for` + ' ' +
+                    `${req.body.number_of_pairs}` + ` ` + `pairs of` + ` ` + `${req.body.shocker_brand}.`
+                    + ` ` + `The service requested is` + ' ' + `${req.body.type_of_service}.` + "\n" +
+                    'Hence,quotation for team' + ` ` + `${req.body.team_name}` + ` ` + `will be mailed to you by EOD.` + "\n" +
+                    `For any further queries, please don't hesitate to get in touch with us at (theshockmechanica@gmail.com or 9068056922).` + "\n" +
+                    `Sincerely,` + "\n" +
+                    `Harsh Jha` + "\n" +
+                    `The Shock Mechanica`
+
+            }
+        }
+        else if (req.body.purchase_form == true) {
+            this.mailOptions = {
+                from: 'Mail.theshockmechanica@gmail.com',
+                to: ['arpitghai20@gmail.com', 'theshockmechanica@gmail.com'],
+                cc: req.body.email,
+                subject: 'Test node mail',
+                text: `Hi` + ' ' + `${req.body.name},` + "\n" +
+                    `This is an automated mail confirming your interest in buying` + ' ' +
+                    `${req.body.number_of_peices}` + ` ` + `peices of` + ` ` + `${req.body.part_type}` + "." + "\n" +
+                    + ' ' + `Hence, quotation for team` + ` ` + `${req.body.team_name}` + ` ` + `will be mailed to you by EOD.` + ' ' +
+                    `For any further queries, please don't hesitate to get in touch with us at (theshockmechanica@gmail.com or 9068056922).` + "\n" +
+                    `Sincerely,` + "\n" +
+                    `Harsh Jha` + "\n" +
+                    `The Shock Mechanica`
+
+            }
+        } else if (req.body.contact_form == true) {
+            this.mailOptions = {
+                from: 'Mail.theshockmechanica@gmail.com',
+                to: ['arpitghai20@gmail.com', 'theshockmechanica@gmail.com'],
+                cc: req.body.email,
+                subject: 'Test node mail',
+                text: `Hi` + ' ' + `${req.body.name}` + "\n" +
+                    `This is an automated mail considering your requirement of` + ' ' + `${req.body.other_requirement}` + "\n" +
+                    `We are looking in to your requirement and will respond you in time. ` + "\n" +
+                    `For any further queries, please don't hesitate to get in touch with us at (theshockmechanica@gmail.com or 9068056922).` + "\n" +
+                    `Sincerely,` + "\n" +
+                    `Harsh Jha` + "\n" +
+                    `The Shock Mechanica`
+
+            }
+        }
+
+
+        nodeMail.sendMail(this.mailOptions, function (error, Info) {
+            if (error) {
+                res.status(400).json(error)
+                console.log(error);
+                res.status(201).json(customerData);
+            } else {
+                if (Info.response) {
+                    res.status(201).json(customerData);
+                    console.log('email has been sent', Info.response);
+                } else {
+                    console.log('email has been sent', Info.rejected);
+
+                    res.status(201).json(customerData);
+                }
+
+            }
         })
 
-        
+    }).catch(err => {
+
+        res.status(400).send("Error" + err)
+    })
 
 
-   
 
-  
+
+
+
+
 });
 
 
 
 router.delete("/:id", async (req, res) => {
     try {
-        console.log('consumer delete request');
-        let consumerData = await customersData.findById(req.params.id)
+        console.log('customer delete request');
+        let customerData = await customersData.findById(req.params.id)
 
-        const deletedConsumer = await consumerData.delete();
-        res.status(200).json(deletedConsumer)
+        const deletedcustomer = await customerData.delete();
+        res.status(200).json(deletedcustomer)
     } catch (err) {
         res.send("err" + err)
     }
@@ -105,13 +175,13 @@ router.delete("/:id", async (req, res) => {
 //     console.log('put request');
 
 //     try{
-//     let consumerData=await consumersData.findById(req.params.id) 
-//         consumerData.usn=req.body.usn;
-//         consumerData.role=req.body.role;
-//         consumerData.dob=req.body.dob;
-//         consumerData.score=req.body.score;
-//         consumerData.name=req.body.name;
-//         consumerData.link=req.body.link;
+//     let customerData=await customersData.findById(req.params.id) 
+//         customerData.usn=req.body.usn;
+//         customerData.role=req.body.role;
+//         customerData.dob=req.body.dob;
+//         customerData.score=req.body.score;
+//         customerData.name=req.body.name;
+//         customerData.link=req.body.link;
 //         const a1=await alien.save()
 //     res.json(a1)
 //     console.log(alien);
