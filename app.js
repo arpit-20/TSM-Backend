c6d613cb45995287987e45c9d607dc5e5d2a1511
Arpit <arp@publicisgroupe.net>
2022-06-10 13:52:06 +0530

    header added

--- app.js
+++ app.js
@@ -6,13 +6,9 @@ const cors=require("cors");
 
 const app=express();
 
-const corsOptions ={
-   origin:'*', 
-   credentials:true,            //access-control-allow-credentials:true
-   optionSuccessStatus:200,
-}
-
-app.use(cors(corsOptions))
+app.use(cors({
+    origin:'*'
+ }))
 
 app.use(express.json());
 
